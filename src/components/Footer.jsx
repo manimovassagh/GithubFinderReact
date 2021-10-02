6ae988aa393d9da9c857e0e7ce7ba509a504852a
manimovassagh <mani.mgh@gmail.com>
2021-10-02 23:20:32 +0200

    refactor done

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Footer() {
+  return (
+    <div>
+      <h1>Just check</h1>
+      <div></div>
+      
+    </div>
+  )
+}
---
+++ src/components/Header.jsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import { Menu } from 'semantic-ui-react'
+import { useState } from "react"
+
+
+export default function MenuExampleBasic() {
+
+  const [activeItem, setaAtiveItem] = useState("")
+  const handleItemClick = () => (e => setaAtiveItem(e.target.name))
+
+
+
+  return (
+    <Menu>
+      <Menu.Item
+        name='editorials'
+        active={activeItem === 'editorials'}
+        onClick={handleItemClick}
+      >
+        Editorials
+      </Menu.Item>
+
+      <Menu.Item
+        name='reviews'
+        active={activeItem === 'reviews'}
+        onClick={handleItemClick}
+      >
+        Reviews
+      </Menu.Item>
+
+      <Menu.Item
+        name='upcomingEvents'
+        active={activeItem === 'upcomingEvents'}
+        onClick={handleItemClick}
+      >
+        Upcoming Events
+      </Menu.Item>
+    </Menu>
+  )
+
+}
